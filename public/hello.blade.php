118a7adbc3c0b69f1b08e0f30e8770758a76144a
dcorry586 <dylancorry15@gmail.com>
2021-08-31 13:12:39 +0100

    Added new view called hello and re-route to it now.

---
+++ public/hello.blade.php
@@ -0,0 +1,8 @@
+<?php
+$coolString = 'noiice';
+?>
+
+
+<h1>{{ $coolString }} </h1>
+
+<p>Learning Laravel 8 </p>
--- public/hello.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php
-    $h1 = '<h1 style="text-align: center;">Hello, Laravel 2K21!</h1>';
-    echo $h1;
-?>
-
